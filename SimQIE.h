a15be14921e5067430401e440975562daf2e45e8
GNiramay <niramaygogate@gmail.com>
2020-08-25 23:04:26 +0530

    added gain and sampling freq.

--- SimQIE.h
+++ SimQIE.h
@@ -1,5 +1,5 @@
 // file to store QIE simulation related functions
-
+// default charge unit = femto C = 1e-15 C
 #include"TMath.h"
 #include"Pulse.h"
 
@@ -10,19 +10,24 @@ public:
   SimQIE(float pd, float sg, bool DBG=false); // with noise & pedestal
 
   float QBins[257];		// DO NOT USE. Problem with oevrlapping regions
-
+  void SetGain(float gg=1e+6);	// set gain of QIE
+  void SetFreq(float sf=40);	// sampling frequency in MHz
+  
   float QErr(float);
   int Q2ADC(float);
   float ADC2Q(int);
 
   int TDC(Pulse*,float);
-  int* Out_ADC(Pulse*,int);	// Output per 25 ns, for N time samples
+  int* Out_ADC(Pulse*,int);	// Output per time sample, for N time samples
   int* CapID(Pulse*, int);	// return CapID for N time samples
 
 private:
   int bins[5] = {0,16,36,57,64};
   float edges[17]={-16, 34, 158, 419, 517, 915, 1910, 3990, 4780, 7960, 15900, 32600, 38900, 64300, 128000, 261000, 350000};
   float sense[16]={3.1, 6.2, 12.4, 24.8, 24.8, 49.6, 99.2, 198.4, 198.4, 396.8, 793.6, 1587, 1587, 3174, 6349, 12700};
+
+  float Gain = 1;		// QIE gain -> to convert from no. of e- to charge in fC
+  float Tau = 25;		// time period of one time sample [in ns]
 
   float TDC_thr = 3.74;				    // TDC threshold - 3.74 microAmpere
   TRandomGen<ROOT::Math::MixMaxEngine<240,0>>* trg; // Random number generator
@@ -43,9 +48,19 @@ SimQIE::SimQIE(float PD, float SG, bool DBG=false)
   sg = SG;
 }
 
+void SimQIE::SetGain(float gg=1e+6)
+{
+  Gain = gg*1600;		// to convert from 1.6e-19 to fC
+}
+
+void SimQIE::SetFreq(float sf = 40)
+{
+  Tau = 1000/sf;		// 1/sf -> MHz to ns
+}
+
 int SimQIE::Q2ADC(float QQ)
 {
-  float qq = QQ;
+  float qq = Gain*QQ;		    // including QIE gain
   if(IsNoise) qq+=trg->Gaus(mu,sg); // Adding gaussian random noise.
 
   if(qq<=edges[0]) return(0);
@@ -115,7 +130,7 @@ int* SimQIE::Out_ADC(Pulse* pp,int N)
   OP[0]=0;			// needs to be changed later
 
   for(int i=0;i<N;i++){
-    float QQ = pp->Integrate(i*25,i*25+25);
+    float QQ = pp->Integrate(i*Tau,i*Tau+Tau);
     OP[i+1]=Q2ADC(QQ);
   }
   return(OP);
@@ -123,8 +138,9 @@ int* SimQIE::Out_ADC(Pulse* pp,int N)
 
 int SimQIE::TDC(Pulse* pp, float T0=0)
 {
+  float thr2=TDC_thr/Gain;
   if(pp->eval(T0)>TDC_thr) return(62);		// when pulse starts high
-  for(float tt=T0;tt<T0+25;tt+=0.1){
+  for(float tt=T0;tt<T0+Tau;tt+=0.1){
     if(pp->eval(tt)>=TDC_thr) return((int)(2*tt));
   }
   return(63);			// when pulse remains low all along
